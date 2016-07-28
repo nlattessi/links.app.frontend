7225099096409acf3ebd85f41e14b301bd2630d6
Nahuel Lattessi <n_lattessi@hotmail.com>
2016-07-27 22:18:08 -0300

    prototype 2

--- bootstrap4/js/app.js
+++ bootstrap4/js/app.js
@@ -1,6 +1,35 @@
+Vue.component('navbar', {
+  template: '#navbar-template'
+})
+
 var vm = new Vue({
-  el: '#app',
+  el: '#app'
+})
+
+var vm = new Vue({
+  el: '#app-links',
   data: {
-    message: 'Hello world!'
+    categories: [
+        { name: 'Cat 1', links: [
+            { url: 'https://www.google.com.ar/?gws_rd=ssl', title: 'Google' },
+            { url: 'https://gist.github.com/nlattessi/26079bb306ad6350159a76657b1eaccd', title: 'https://gist.github.com/nlattessi/26079bb306ad6350159a76657b1eaccd' },
+        ]},
+        { name: 'Cat 2', links: [
+            { url: 'https://www.gog.com/', title: 'Gog' },
+            { url: 'http://store.steampowered.com/', title: 'http://store.steampowered.com/' },
+        ]},
+    ]
   }
 })
+
+var vm2 = new Vue({
+  el: '#vue-instance',
+  data: {
+    inventory: [
+      {name: 'MacBook Air', price: 1000},
+      {name: 'MacBook Pro', price: 1800},
+      {name: 'Lenovo W530', price: 1400},
+      {name: 'Acer Aspire One', price: 300}
+    ]
+  }
+});
